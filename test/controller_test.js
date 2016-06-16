0728599ab40d42149d3b7fafb3769328712103ab
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-16 10:53:43 -0700

    got constructer tests working and now testing functions

--- test/controller_test.js
+++ test/controller_test.js
@@ -5,7 +5,54 @@ require('../app/js/client.js');
 require('angular-mocks');
 
 describe('controller tests', () => {
-  it('shoudl work', () => {
-    expect(true).toBe(true);
+  let drew;
+
+  beforeEach(() => {
+    angular.mock.module('adventureApp');
+    angular.mock.inject(function($controller) {
+      drew = new $controller('GameController');
+    });
+  });
+
+  it('should have a property gamerLocation', () => {
+    expect(drew.gamerLocation).toBe('start');
+  });
+
+  it('should start a game', () => {
+    expect(drew.gamelog.length).toBe(0);
+  });
+
+  it('should have a property gamerHasSwag', () => {
+    expect(drew.gamerHasSwag).toBe(false);
+  });
+
+  it('should have a property gamerHasRubies', () => {
+    expect(drew.gamerHasRubies).toBe(false);
+  });
+
+  it('should have a property command', () => {
+    expect(drew.command).toBe('');
+  });
+
+  it('should expect location to be an object', () => {
+    expect(typeof drew.location).toBe('object');
+  });
+
+  it('should expect start location prompt to be a string', () => {
+    expect(typeof drew.location.start.prompt).toBe('string');
+  });
+
+  describe('startGame fucntion tests', () => {
+    beforeEach(() => {
+      drew.startGame();
+    });
+
+    it('startGame function should prompt correctly work', () => {
+      expect(drew.location.start.prompt).toBe('Welcome to Swag Survivor. You are in a room with the Swag Killer monster.');
+    });
+
+    it('gamelog should have things in it', () => {
+      expect(drew.gamelog.length).not.toBe(-1);
+    });
   });
 });
